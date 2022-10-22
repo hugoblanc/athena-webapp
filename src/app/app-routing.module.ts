0f734d4c196bec50e5082d82a24c7dd04e50029e
Hugo Blanc <hugo.blanc@agicap.com>
2022-10-22 18:31:28 +0200

    feat: redirect to home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) }, { path: 'privacy', loadChildren: () => import('./privacy/privacy.module').then(m => m.PrivacyModule) }, { path: 'share', loadChildren: () => import('./share/share.module').then(m => m.ShareModule) }];
+const routes: Routes = [
+  { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
+  { path: 'privacy', loadChildren: () => import('./privacy/privacy.module').then(m => m.PrivacyModule) }, { path: 'share', loadChildren: () => import('./share/share.module').then(m => m.ShareModule) },
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
